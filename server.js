8d5ff75ca644ba93ae3afd2b416263109f987488
lordneru <lordneru@gmail.com>
2018-03-15 21:54:48 +0100

    more fix

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var app = express;
+var app = express();
 
 app.use(express.static('www'));
 
 app.set('port', process.env.PORT || 500);
 app.listen(app.get('port'), function(){
-    console.log('Express ionic server listing onport ' + app.get('port'));
+    console.log('Express ionic gitserver listing onport ' + app.get('port'));
 })
